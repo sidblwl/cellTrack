6bc421ff6843a0744cf3dfd1701396311b96257f
Tejas Thiyagarajan <tejas.thiyagarajan@gmail.com>
2024-06-26 16:45:39 -0400

    added material design UI

--- client/src/components/CreateLine.jsx
+++ client/src/components/CreateLine.jsx
@@ -1,6 +1,8 @@
 import '../App.css'
 import { Routes, Route, useParams, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
+
 
 export default function CreateLine(){
 
@@ -26,12 +28,15 @@ export default function CreateLine(){
     return (
         <>
             <h1>Create new line</h1>
-            <p>Name:</p>
-            <input id="nameIn"></input>
-            <p>Passage Num:</p>
-            <input id="numIn"></input><br></br><br></br>
-            <Link to={`/`} onClick={() => {addCellLine(document.getElementById("nameIn").value, document.getElementById("numIn").value)}}>
-                <h1>Create New Line</h1>
+            <p>Cell Line</p>
+            <input class="material-input" id="nameIn"></input>
+            <p>Passage #</p>
+            <input class="material-input" id="numIn"></input>
+            <br></br>
+            <Link to={'/'}>
+                <Button variant="contained" className="modern-button" onClick={() => {addCellLine(document.getElementById("nameIn").value, document.getElementById("numIn").value)}}>
+                    Create New Cell Line
+                </Button>
             </Link>
         </>
     )
