ac776280a2e63c07c774be2427ed02fb066f3a31
Siddharth Belwal <siddharth.belwal@gmail.com>
2024-07-08 14:08:33 -0700

    rearranged file structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { useState } from 'react'
